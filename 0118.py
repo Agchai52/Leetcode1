05ad56960b95a3cbf4e65546634496975e994a18
SZhang <37639111+Agchai52@users.noreply.github.com>
2019-05-06 17:42:51 -0400

    Create 0118.py

---
+++ 0118.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        triangle = []
+        for i in range(numRows):
+            row = [None] * (i+1)
+            row[0], row[-1] = 1, 1
+            for j in range(1, len(row)-1):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            
+            triangle.append(row)
+        return triangle
