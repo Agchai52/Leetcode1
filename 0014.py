f4bc24c5303ed89238b3f2f83575d1e1769eab5c
SZhang <37639111+Agchai52@users.noreply.github.com>
2019-05-07 17:45:43 -0400

    Create 0014.py

---
+++ 0014.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        res = ""
+        for i in xrange(len(strs[0])):
+            for j in xrange(1,len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        
+        return res
+    
