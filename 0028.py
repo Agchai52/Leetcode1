46fc1c6c9950c33d3017063d80d10e7815fff8bf
SZhang <37639111+Agchai52@users.noreply.github.com>
2019-05-07 17:20:08 -0400

    Create 0028.py

---
+++ 0028.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        elif needle in haystack:
+            for i in range(len(haystack)-len(needle)+1):
+                if haystack[i:i+len(needle)] == needle:
+                           return i
+                    
+        else:
+            return -1
