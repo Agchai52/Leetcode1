67fbf18e59963fa43695c655613cc7879c03039d
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-05-30 15:10:42 -0400

    Create 0274.py

---
+++ 0274.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # Sort and Binary
+        citations.sort()
+        n, left, right = len(citations), 0, len(citations)-1
+        
+        while left <= right:
+            mid = (left + right) / 2
+            if citations[mid] >= n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return n-left
