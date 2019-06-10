f7b1fb58388b61804ecbbb56be9d31a11b2c3c27
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-06-10 17:22:39 -0400

    Create 0374.py

---
+++ 0374.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            mid = (left + right) / 2
+            if guess(mid) == 0:
+                return mid
+            elif guess(mid) == 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
