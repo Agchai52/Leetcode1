5b9f22cc17044d84b966ff57077276ada5b74b11
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-06-22 22:19:40 -0400

    Create 0064.py

---
+++ 0064.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        dp = grid
+        
+        for i in xrange(m):
+            for j in xrange(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        
+        return dp[-1][-1]
+                
