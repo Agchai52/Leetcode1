7ba7ede8bb427e0ac7dfaad2e2bf87051490c47a
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-07 17:46:26 -0400

    Create 0117.py

---
+++ 0117.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root: return
+        q = [root]
+        while q:
+            l = len(q)
+            for i in range(l):
+                node = q.pop(0)
+                if i < l-1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
