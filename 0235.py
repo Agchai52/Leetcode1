1b8f8fedce6d0cbcb8971e491d9d5e76f3a191d8
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-07 12:20:47 -0400

    Create 0235.py

---
+++ 0235.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if p.val > q.val: # ensure p.val < q.val
+            temp = p
+            p = q
+            q = temp
+        
+        key = root.val
+        
+        if p.val <= key and q.val >= key:
+            return root
+        elif q.val < key:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > key:
+            return self.lowestCommonAncestor(root.right, p, q)
