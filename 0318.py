aa2f126e134d909aff81b2e52346ded115e88d3c
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-11 15:26:26 -0400

    Create 0318.py

---
+++ 0318.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        res = 0
+        d = collections.defaultdict(int)
+        for i in range(len(words)):
+            w = words[i]
+            for c in w:
+                d[w] |= 1 << (ord(c) - ord('a')) # a:1, b:10, c:100, ...            
+            for j in range(i):
+                if not d[words[j]] & d[words[i]]: # two words don't share common letters
+                    res = max(res, len(words[j]) * len(words[i]))
+        return res
+                
