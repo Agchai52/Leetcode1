f845339519eb4428cb138ce088f83e70021e1d3b
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-12 21:38:36 -0400

    Create 0138.py

---
+++ 0138.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        nodeDict = dict()
+        dummy = Node(0, None, None)
+        nodeDict[head] = dummy
+        newHead, pointer = dummy, head
+        while pointer:
+            node = Node(pointer.val, pointer.next, None)
+            nodeDict[pointer] = node
+            newHead.next = node
+            newHead, pointer = newHead.next, pointer.next
+        
+        pointer = head
+        while pointer:
+            if pointer.random:
+                nodeDict[pointer].random = nodeDict[pointer.random]
+            pointer = pointer.next
+            
+        return dummy.next
