02938814a7284eb02853ed6da1d155d318809aa9
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-15 18:21:28 -0400

    Create 0011.py

---
+++ 0011.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, h):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(h)-1
+        res = 0
+        while left < right:
+            res = max(res, min(h[left], h[right])*(right - left))
+            if h[left] < h[right]:
+                left += 1
+            else:
+                right -= 1
+        return res
+        
