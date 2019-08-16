9370702e24f49c3c8ad2e7bf2efe610fd9b530f1
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-15 21:52:01 -0400

    Create 0056.py

---
+++ 0056.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        for interval in sorted(intervals, key = lambda x: x[0]):
+            if not res or interval[0] > res[-1][1]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
+        
