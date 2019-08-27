b90722aeb7da4657cd5f9eaf26508d5e6a89aed2
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-26 23:22:24 -0400

    Create 0297.py

--- 0297.py
+++ 0297.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#class TreeNode(object):
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
 
 class Codec:
 
@@ -13,22 +13,28 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if not root: return '[]'
-        res = [str(root.val)]
-        q = [root]        
-        while q:            
-            node = q.pop(0)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-            res.append(str(node.left.val) if node.left else 'null')
-            res.append(str(node.right.val) if node.right else 'null')
-        while res and res[-1] == 'null':
-            res.pop()
-        return '[' + ','.join(res) + ']'
+        # Method 1: BFS
+        #if not root: return '[]'
+        #res = [str(root.val)]
+        #q = [root]        
+        #while q:            
+        #    node = q.pop(0)
+        #    if node.left:
+        #        q.append(node.left)
+        #    if node.right:
+        #        q.append(node.right)
+        #    res.append(str(node.left.val) if node.left else 'null')
+        #    res.append(str(node.right.val) if node.right else 'null')
+        #while res and res[-1] == 'null':
+        #    res.pop()
+        #return '[' + ','.join(res) + ']'
             
-        
+        # Method 2: DFS
+        if not root: return [None]
+        res = [root.val]
+        left = self.serialize(root.left)
+        right = self.serialize(root.right)
+        return res + left + right
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -36,28 +42,38 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if data == '[]': return None
-        nums = data[1:-1].split(',')
-        root = TreeNode(int(nums.pop(0)))
-        q = [root]
-        while len(q) != 0 and len(nums) !=0:
-            cur = q.pop(0)
-            val = nums.pop(0)
-            if val != 'null':
-                node = TreeNode(int(val))
-                q.append(node)
-                cur.left = node
-            if len(nums) !=0:
-                val = nums.pop(0)
-            else:
-                break
-            if val != 'null':
-                node = TreeNode(int(val))
-                q.append(node)
-                cur.right = node
-        return root
+        # Method 1:
+        #if data == '[]': return None
+        #nums = data[1:-1].split(',')
+        #root = TreeNode(int(nums.pop(0)))
+        #q = [root]
+        #while len(q) != 0 and len(nums) !=0:
+        #    cur = q.pop(0)
+        #    val = nums.pop(0)
+        #    if val != 'null':
+        #        node = TreeNode(int(val))
+        #        q.append(node)
+        #        cur.left = node
+        #    if len(nums) !=0:
+        #        val = nums.pop(0)
+        #    else:
+        #        break
+        #    if val != 'null':
+        #        node = TreeNode(int(val))
+        #        q.append(node)
+        #        cur.right = node
+        #return root
        
-        
+        # Method 2: DFS
+        if not data: return None        
+        val = data.pop(0)
+        if val == None:
+            return None
+        root = TreeNode(val)
+        root.left = self.deserialize(data)
+        root.right = self.deserialize(data)
+        return root
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
