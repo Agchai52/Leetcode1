f0002a36217938544b54fa80d54bdc1895534dae
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2019-08-29 15:04:39 -0400

    Create 0409.py

--- 0409.py
+++ 0409.py
@@ -4,14 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Method 1: Hash Table
+        #if not s: return 0
+        #n = len(s)
+        #d = collections.Counter(s)
+        #extra = 0
+        #for k in d:
+        #    if d[k] % 2 == 1:
+        #        extra += 1
+        #if extra == 0: return n
+        #else: return n-extra+1
+        
+        # Method 2: stack
         if not s: return 0
         n = len(s)
-        d = collections.Counter(s)
-        extra = 0
-        for k in d:
-            if d[k] % 2 == 1:
-                extra += 1
-        if extra == 0: return n
-        else: return n-extra+1
-
+        s = [ord(c) for c in s]
+        s.sort()
+        stack = [s[0]]
+        for i in range(1, n):
+            if stack and s[i] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        if not stack: return n
+        else: return n - len(stack) + 1
     
