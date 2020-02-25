0657f474935746f12d976ea7cbcfed5d162a9060
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-02-25 18:31:14 -0500

    Create 0189.py

--- 0189.py
+++ 0189.py
@@ -5,6 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        '''
+        # Method 1: Extra Space T=O(N), S=O(N) 
         n = len(nums)
         a = [None]*n
         for i in range(n):
@@ -12,3 +14,38 @@ class Solution(object):
             a[shift] = nums[i]
         for j in range(n):
             nums[j] = a[j]
+        '''
+        '''
+        # Method 2: In-place, recycle T=O(N), S=O(1)
+        k = k % len(nums)
+        count = 0
+        start = 0
+        while count < len(nums):
+            current = start
+            prev = nums[start]
+            
+            while True:
+                next = (current + k) % len(nums)
+                temp = nums[next]
+                nums[next] = prev
+                prev = temp
+                current = next
+                count += 1
+                if start == current: 
+                    break
+            start += 1
+        '''
+        # Method 3: reverse, T=O(n), S=O(1)
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+            
