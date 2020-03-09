54c2981544cc1547307b1f4edd0a073ffc94c157
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-03-09 19:59:23 -0400

    Create 349.py

---
+++ 349.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set1, set2 = set(nums1), set(nums2)
+        return list(set1.intersection(set2))
