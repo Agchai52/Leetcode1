55dc54e7e6de300f566bdfc77bc1c64e621697e8
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-03-10 10:55:28 -0400

    Delete 349.py

--- 349.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def intersection(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        set1, set2 = set(nums1), set(nums2)
-        return list(set1.intersection(set2))
