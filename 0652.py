4985b059660a6f1d79204f2fbcf8aca291133fcb
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-03-20 15:24:35 -0400

    Create 0652.py

---
+++ 0652.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[TreeNode]
+        """
+        count = collections.Counter()
+        ans = []
+        def collect(node):
+            if not node: return '#'
+            serial = '{}, {}, {}'.format(node.val, collect(node.left), collect(node.right))
+            count[serial] += 1
+            if count[serial] == 2:
+                ans.append(node)
+            return serial
+        
+        collect(root)
+        return ans
