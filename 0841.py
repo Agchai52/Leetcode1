7af3a08805437bd2e3a8e0ffcbcdc0ab5b4a5599
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-05-16 22:59:29 -0400

    Create 0841.py

---
+++ 0841.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        seen = [False] * len(rooms)
+        seen[0] = True
+        stack = [0]
+        
+        while stack:
+            node = stack.pop()
+            for key in rooms[node]:
+                if not seen[key]:
+                    seen[key] = True
+                    stack.append(key)
+        return all(seen)
